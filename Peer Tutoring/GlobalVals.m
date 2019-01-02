3d9667d7549c77fa1d31d2d277e60d4360246efa
Sam Noyes <snoyes1234@gmail.com>
2019-01-01 21:52:00 -0700

    Edited TODO

--- Peer Tutoring/GlobalVals.m
+++ Peer Tutoring/GlobalVals.m
@@ -2,7 +2,7 @@
 //Edits to undo:
 // * QuestionDetailViewController.m line 58
 //TODO:
-//Change AskQuestionViewController to TableViewController? So the text entry would be on a separate page rather than embedded in the current one.
+//Add refreshing functionality to question list.
 //Fix commenting. Maybe add a separate CommentDetailViewController where the user could see an expanded version of the comment and reply?
 //Related: add replying to comments
 //Add login/signup page
